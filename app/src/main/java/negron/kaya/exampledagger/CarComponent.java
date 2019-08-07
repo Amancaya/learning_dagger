9e7263e6cefbdc310e812aeb7a26e609e1da1a80
kaya96 <koda01996@gmail.com>
2019-08-07 13:54:29 -0400

    learning about component.builder bindsInstance and named

--- app/src/main/java/negron/kaya/exampledagger/CarComponent.java
+++ app/src/main/java/negron/kaya/exampledagger/CarComponent.java
@@ -1,13 +1,31 @@
 package negron.kaya.exampledagger;
 
+import javax.inject.Named;
+
+import dagger.Binds;
+import dagger.BindsInstance;
 import dagger.Component;
 import negron.kaya.exampledagger.dagger.DieselEngineModule;
+import negron.kaya.exampledagger.dagger.PetrolEngineModule;
 import negron.kaya.exampledagger.models.Car;
+import negron.kaya.exampledagger.models.PetrolEngine;
 
-@Component(modules = {WheelsModule.class, DieselEngineModule.class})
+@Component(modules = {WheelsModule.class, PetrolEngineModule.class})
 public interface CarComponent {
 
     Car getCar();
 
     void inject(MainActivity mainActivity);
+
+    @Component.Builder
+    interface BuilderOther {
+
+        @BindsInstance
+        BuilderOther horsePower(@Named("horse") int horsePower);
+
+        @BindsInstance
+        BuilderOther engineCapacity(@Named("engine") int engineCapacity);
+
+        CarComponent build();
+    }
 }
--- app/src/main/java/negron/kaya/exampledagger/models/PetrolEngine.java
+++ app/src/main/java/negron/kaya/exampledagger/models/PetrolEngine.java
@@ -3,16 +3,25 @@ package negron.kaya.exampledagger.models;
 import android.util.Log;
 
 import javax.inject.Inject;
+import javax.inject.Named;
 
 public class PetrolEngine implements Engine {
 
     private static final String TAG = "Car";
 
+    private int horsePower;
+    private int engineCapacity;
+
     @Inject
-    public PetrolEngine(){ }
+    public PetrolEngine(@Named("horse") int horsePower, @Named("engine") int engineCapacity){
+        this.horsePower = horsePower;
+        this.engineCapacity = engineCapacity;
+    }
 
     @Override
     public void start() {
-        Log.d(TAG, "Petrol engine started");
+        Log.d(TAG, "Petrol engine started. " +
+                "\nHorsepower: " + horsePower +
+                "\nEngine capacity: " + engineCapacity);
     }
 }
